fcfbe8519c251aa24332ff61253566318d7c76d9
tslamic <tadej.slamic@gmail.com>
2015-08-12 09:15:02 +0100

    Slight method profiling adjustment.

--- lib/Adn/app/src/main/java/tslamic/github/io/adn/MainActivity.java
+++ lib/Adn/app/src/main/java/tslamic/github/io/adn/MainActivity.java
@@ -9,31 +9,45 @@ import tslamic.github.io.androiddevicenames.DeviceNames;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "DeviceNames";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         final TextView deviceName = (TextView) findViewById(R.id.device_name);
         deviceName.setText(DeviceNames.getThisDeviceName("Unknown Device"));
+
         new Thread(new Profile()).start();
     }
 
-    private class Profile implements Runnable {
+    private static class Profile implements Runnable {
+
+        private static final double MILLISECOND_IN_NANOS = 1000000.0;
+        private static final int COUNT = 10000;
+
         @Override
         public void run() {
             long duration = 0L;
 
-            final int count = 10000;
-            for (int i = 0; i < count; i++) {
-                final String name = "failure" + i;
+            // Warm up
+            for (int i = 0; i < 1000; i++) {
+                final String name = "should_fail" + i;
+                DeviceNames.getDeviceName(name, null);
+            }
+
+            // Measure
+            for (int i = 0; i < COUNT; i++) {
+                final String name = "should_fail" + i;
                 long start = System.nanoTime();
                 DeviceNames.getDeviceName(name, null);
                 duration += (System.nanoTime() - start);
             }
 
-            final double avg = ((duration / count) / 1000000.0);
-            final String msg = String.format("Avg duration for %d iterations: %f ms.", count, avg);
-            Log.i("DeviceNames", msg);
+            final double avg = duration / (COUNT * MILLISECOND_IN_NANOS);
+            final String msg = String.format("Average worst case scenario duration" +
+                    " after %d iterations: %.3f ms.", COUNT, avg);
+            Log.i(TAG, msg);
         }
     }
 
